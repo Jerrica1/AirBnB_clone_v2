3aa30a7b0e5eadc51d031f59a74215d9886a8931
Jerrica1 <jerricajackson93@outlook.com>
2023-06-20 07:16:43 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+"""class User is defined here to inherit from BaseModel"""
+import models
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+class User(BaseModel):
+    """class User inherits from BaseModel
+    Arguments:
+        email = empty string
+        password = emtpy string
+        first_name = empty string
+        last_name = empty string
     """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+
+    # public instances
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
